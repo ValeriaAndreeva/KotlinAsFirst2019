92d9e451bcc2101556c275a43f83c8cb93672b54
andreevavalerya <valeria.andreeva.2013@mail.ru>
2019-10-20 15:35:51 +0300

    lesson1/lesson2-corrected

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -155,11 +155,12 @@ fun rookOrBishopThreatens(
      */
     fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
         return when {
-            ((a >= d) || (c >= d)) -> -1
+            ((a > d) || (c > d)) -> -1
             ((a <= c) && (d <= b)) -> d - c
             ((c < a) && (d < b)) -> d - a
             ((a < c) && (b < d)) -> b - c
             ((c <= a) && (b <= d)) -> b - a
+            ((a == b) && (c == d)) -> 0
             else -> 0
         }
     }
